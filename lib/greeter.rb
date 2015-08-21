409219d78d257ed7e1b2c54e299260f318e73851
hilpitome <hilpitome@gmail.com>
2015-08-21 20:51:53 +0300

    defined render method

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -1,8 +1,15 @@
-
+rewquire "erb"
 
 class Greeter
 
   def call(env)
-  Rack::Response.new("Hello mike")
+  Rack::Response.new(render("index.html.erb"))
   end
+
+  def render(template)
+
+    path = File.expand_path("../views/#{template}"_FILE_)
+    ERB.new(File.read(path)).result(binding)
+  end
+
  end
