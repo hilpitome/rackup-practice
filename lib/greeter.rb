fb60f8114db39d38f0371c319238c012657b52b9
hilpitome <hilpitome@gmail.com>
2015-08-22 12:18:55 +0300

    modified greeter torespond to form input

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -6,6 +6,7 @@ class Greeter
     request = Rack::Request.new(env)
     case request.path
     when "/" then  Rack::Response.new(render("index.html.erb"))
+    when "/change"  Rack::Response.new(request.params["name"])
     else Rack::Response.new("Not Found", 404)
     end
 
