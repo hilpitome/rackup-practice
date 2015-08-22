b9a70d142cb64f9ddb82898daae62c9b2bb2dc92
hilpitome <hilpitome@gmail.com>
2015-08-22 12:08:44 +0300

    created a 404 for wrong path entered

--- lib/greeter.rb
+++ lib/greeter.rb
@@ -4,7 +4,11 @@ class Greeter
 
   def call(env)
     request = Rack::Request.new(env)
-  Rack::Response.new(render("index.html.erb"))
+    case request.path
+    when "/" then  Rack::Response.new(render("index.html.erb"))
+    else Rack::Response.new("Not Found", 404)
+    end
+
   end
 
   def render(template)
@@ -12,5 +16,4 @@ class Greeter
     path = File.expand_path("../views/#{template}", __FILE__)
     ERB.new(File.read(path)).result(binding)
   end
-
  end
