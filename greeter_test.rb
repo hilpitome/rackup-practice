bbabe8d61bc1f5af8f62732e900be7b14f66a8c1
hilpitome <hilpitome@gmail.com>
2015-08-23 22:15:00 +0300

    passed in app to test

--- greeter_test.rb
+++ greeter_test.rb
@@ -2,3 +2,9 @@ require "rubygems"
 require "rack"
 require "minitest/autorun"
 require File.expand_path("..lib/greeter", __FILE__)
+
+describe Greeter do
+  before do
+    @request = Rack::MockRequest.new(Greeter)
+  end
+end
