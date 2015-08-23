18ce875b03e510c9f0750db3774061bd77077012
hilpitome <hilpitome@gmail.com>
2015-08-23 22:18:11 +0300

    created unknown response test

--- greeter_test.rb
+++ greeter_test.rb
@@ -6,5 +6,8 @@ require File.expand_path("..lib/greeter", __FILE__)
 describe Greeter do
   before do
     @request = Rack::MockRequest.new(Greeter)
+  it "returns 404 response for unknown requests" do
+    @request.get("/unknown").status.must_equal 404
   end
+
 end
